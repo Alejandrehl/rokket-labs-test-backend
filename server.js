0f92e747a31f3f737906a164c9fdc0e8d9c0ad17
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-12-19 11:14:33 -0300

    DB config

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("ROKKET LABS")
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.use(express.json({ extended: false }));
+// app.use("/api/users", require("./routes/users"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
