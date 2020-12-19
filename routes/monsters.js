31d9b9b25d6ac8ffd99b13f6b13339f5836dee33
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-12-19 11:37:15 -0300

    Monster crud done

--- routes/monsters.js
+++ routes/monsters.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const Monster = require("../models/Monster");
 
+// @route   GET api/monsters
+// @desc    Get all monsters
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const monsters = await Monster.find().sort({
+      createdAt: -1,
+    });
+    res.json(monsters);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 // @route   POST api/monsters
 // @desc    Create a monster
 // @access  Public
@@ -34,5 +48,21 @@ router.post(
     }
   }
 );
+
+// @route   DELETE api/monsters/:id
+// @desc    Delete monster
+// @access  Public
+router.delete("/:id", async (req, res) => {
+  try {
+    let monster = await Monster.findById(req.params.id);
+
+    if (!monster) return res.status(404).json({ msg: "Monster not found." });
+
+    await Monster.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Monster removed.", id: req.params.id });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 module.exports = router;
